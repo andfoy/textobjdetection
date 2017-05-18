e5176c7072febf99a5679cd1c7fe0e8756732feb
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-17 20:02:49 -0500

    LSTM: Update data loading

--- lstm.py
+++ lstm.py
@@ -1,18 +1,22 @@
+
 import argparse
 import time
 import math
 import torch
 import torch.nn as nn
+import os.path as osp
 from torch.autograd import Variable
 
-import data
+# import data
 import model
 
-parser = argparse.ArgumentParser(description='PyTorch PennTreeBank RNN/LSTM Language Model')
+parser = argparse.ArgumentParser(
+    description='PyTorch VisualGenome RNN/LSTM Language Model')
 parser.add_argument('--data', type=str, default='./data/penn',
                     help='location of the data corpus')
 parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
+                    help='type of recurrent net '
+                         '(RNN_TANH, RNN_RELU, LSTM, GRU)')
 parser.add_argument('--emsize', type=int, default=200,
                     help='size of word embeddings')
 parser.add_argument('--nhid', type=int, default=200,
@@ -35,27 +39,35 @@ parser.add_argument('--tied', action='store_true',
                     help='tie the word embedding and softmax weights')
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
-parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
+parser.add_argument('--no-cuda', action='store_true',
+                    help='disables CUDA training')
 parser.add_argument('--log-interval', type=int, default=200, metavar='N',
                     help='report interval')
-parser.add_argument('--save', type=str,  default='model.pt',
+parser.add_argument('--corpus', type=str, default='',
+                    help='path to file that contains'
+                    ' the text corpus dictionary')
+parser.add_argument('--save', type=str, default='model.pt',
                     help='path to save the final model')
 args = parser.parse_args()
 
 # Set the random seed manually for reproducibility.
-torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    if not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-    else:
-        torch.cuda.manual_seed(args.seed)
+args.cuda = not args.no_cuda and torch.cuda.is_available()
 
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
 ###############################################################################
 # Load data
 ###############################################################################
 
-corpus = data.Corpus(args.data)
+with open(args.corpus, 'rb') as f:
+    corpus = torch.load(f)
+
+train_corpus = corpus.tokenize_file(osp.join(args.data, 'train.txt'))
+val_corpus = corpus.tokenize_file(osp.join(args.data, 'val.txt'))
+test_corpus = corpus.tokenize_file(osp.join(args.data, 'test.txt'))
+# corpus = data.Corpus(args.data)
+
 
 def batchify(data, bsz):
     # Work out how cleanly we can divide the dataset into bsz parts.
@@ -68,17 +80,19 @@ def batchify(data, bsz):
         data = data.cuda()
     return data
 
+
 eval_batch_size = 10
-train_data = batchify(corpus.train, args.batch_size)
-val_data = batchify(corpus.valid, eval_batch_size)
-test_data = batchify(corpus.test, eval_batch_size)
+train_data = batchify(train_corpus, args.batch_size)
+val_data = batchify(val_corpus, eval_batch_size)
+test_data = batchify(test_corpus, eval_batch_size)
 
 ###############################################################################
 # Build the model
 ###############################################################################
 
 ntokens = len(corpus.dictionary)
-model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied)
+model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid,
+                       args.nlayers, args.dropout, args.tied)
 if args.cuda:
     model.cuda()
 
@@ -88,8 +102,10 @@ criterion = nn.CrossEntropyLoss()
 # Training code
 ###############################################################################
 
+
 def repackage_hidden(h):
-    """Wraps hidden states in new Variables, to detach them from their history."""
+    """Wraps hidden states in new Variables,
+       to detach them from their history."""
     if type(h) == Variable:
         return Variable(h.data)
     else:
@@ -98,8 +114,8 @@ def repackage_hidden(h):
 
 def get_batch(source, i, evaluation=False):
     seq_len = min(args.bptt, len(source) - 1 - i)
-    data = Variable(source[i:i+seq_len], volatile=evaluation)
-    target = Variable(source[i+1:i+1+seq_len].view(-1))
+    data = Variable(source[i:i + seq_len], volatile=evaluation)
+    target = Variable(source[i + 1:i + 1 + seq_len].view(-1))
     return data, target
 
 
@@ -127,15 +143,21 @@ def train():
     hidden = model.init_hidden(args.batch_size)
     for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
         data, targets = get_batch(train_data, i)
-        # Starting each batch, we detach the hidden state from how it was previously produced.
-        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        """
+        Starting each batch, we detach the hidden state from
+        how it was previously produced.
+        If we didn't, the model would try backpropagating
+        all the way to start of the dataset.
+        """
         hidden = repackage_hidden(hidden)
         model.zero_grad()
         output, hidden = model(data, hidden)
         loss = criterion(output.view(-1, ntokens), targets)
         loss.backward()
-
-        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        """
+        `clip_grad_norm` helps prevent the exploding
+        gradient problem in RNNs / LSTMs.
+        """
         torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
         for p in model.parameters():
             p.data.add_(-lr, p.grad.data)
@@ -145,12 +167,15 @@ def train():
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss[0] / args.log_interval
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f}'.format(
-                epoch, batch, len(train_data) // args.bptt, lr,
-                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            print('| epoch {:3d} | {:5d}/{:5d} batches '
+                  '| lr {:02.2f} | ms/batch {:5.2f} | '
+                  'loss {:5.2f} | ppl {:8.2f}'.format(
+                      epoch, batch, len(train_data) // args.bptt, lr,
+                      elapsed * 1000 / args.log_interval, cur_loss,
+                      math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
+
 
 # Loop over epochs.
 lr = args.lr
@@ -158,14 +183,15 @@ best_val_loss = None
 
 # At any point you can hit Ctrl + C to break out of training early.
 try:
-    for epoch in range(1, args.epochs+1):
+    for epoch in range(1, args.epochs + 1):
         epoch_start_time = time.time()
         train()
         val_loss = evaluate(val_data)
         print('-' * 89)
         print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                           val_loss, math.exp(val_loss)))
+              'valid ppl {:8.2f}'.format(
+                  epoch, (time.time() - epoch_start_time),
+                  val_loss, math.exp(val_loss)))
         print('-' * 89)
         # Save the model if the validation loss is the best we've seen so far.
         if not best_val_loss or val_loss < best_val_loss:
@@ -173,7 +199,10 @@ try:
                 torch.save(model, f)
             best_val_loss = val_loss
         else:
-            # Anneal the learning rate if no improvement has been seen in the validation dataset.
+            """
+            Anneal the learning rate if no improvement
+            has been seen in the validation dataset.
+            """
             lr /= 4
 except KeyboardInterrupt:
     print('-' * 89)
