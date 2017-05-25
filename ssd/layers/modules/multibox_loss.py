7fee1cc924ed686ed04934515248c427e12fdc29
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-24 19:59:16 -0500

    SSD: Update __init__ imports

--- ssd/layers/modules/multibox_loss.py
+++ ssd/layers/modules/multibox_loss.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-from data import v2 as cfg
+from ssd import v2 as cfg
 from ..box_utils import match, log_sum_exp
 GPU = False
 if torch.cuda.is_available():
