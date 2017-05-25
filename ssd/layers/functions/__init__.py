9cc2a7769ae042ac9d7f69d3c0915229859cfe01
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-24 19:50:35 -0500

    SSD: Relative imports

--- ssd/layers/functions/__init__.py
+++ ssd/layers/functions/__init__.py
@@ -1,5 +1,5 @@
-# from .detection import Detect
-# from .prior_box import PriorBox
+from .detection import Detect
+from .prior_box import PriorBox
 
 
-# __all__ = ['Detect', 'PriorBox']
+__all__ = ['Detect', 'PriorBox']
