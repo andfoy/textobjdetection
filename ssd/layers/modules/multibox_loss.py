f04c69c50311d65499ecd09ade56d68dc383158e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-26 18:51:49 -0500

    Train: Debugging

--- ssd/layers/modules/multibox_loss.py
+++ ssd/layers/modules/multibox_loss.py
@@ -88,7 +88,7 @@ class MultiBoxLoss(nn.Module):
         pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_data)
         loc_p = loc_data[pos_idx].view(-1,4)
         loc_t = loc_t[pos_idx].view(-1,4)
-        print(log_p)
+        print(loc_p)
         loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
 
         # Compute max conf across batch for hard negative mining
