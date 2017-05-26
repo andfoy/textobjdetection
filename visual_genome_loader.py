3b1ccf5bfa63145c44bb921fd0d089bf2d66669a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-26 00:11:21 -0500

    Collate: Variables should be allocated on CUDA?

--- visual_genome_loader.py
+++ visual_genome_loader.py
@@ -38,7 +38,7 @@ def detection_collate(batch, rnnmodel):
         imgs.append(img)
         targets.append(torch.stack([torch.Tensor(a) for a in target], 0))
         hidden = rnnmodel.init_hidden(phrase.size(0))
-        _, hidden = rnnmodel(Variable(phrase.cuda()), hidden.cuda())
+        _, hidden = rnnmodel(Variable(phrase.cuda()), hidden)
         phrases.append(hidden)
     return torch.stack(imgs, 0), targets, torch.stack(phrases, 0)
 
