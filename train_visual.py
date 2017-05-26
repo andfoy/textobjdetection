d00f23a4b964b23a4b09ebf3ba916270d157e939
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-26 18:37:05 -0500

    Train: Debugging

--- train_visual.py
+++ train_visual.py
@@ -199,7 +199,7 @@ def train(epoch):
             cur_conf_loss = conf_loss / args.log_interval
 
             # '| loc loss {:.6f} | conf loss: {:.6f}'
-            print('| epoch {:3d} | {:5d}/{:5d} batches '
+            print('| epoch {} | {}/{} batches '
                   '| ms/batch {:5.2f} | total loss {:5.2f} '.format(
                       epoch, batch_idx, len(trainset), elapsed_time * 1000,
                       cur_total_loss, cur_loc_loss, cur_conf_loss))
