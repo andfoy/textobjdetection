d5ba9ef84231d0bce5f3c79304d05f50082718a9
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-27 16:20:37 -0500

    Training: Debugging

--- ssd/layers/box_utils.py
+++ ssd/layers/box_utils.py
@@ -113,7 +113,7 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
     conf = labels[best_truth_idx] + 1         # Shape: [num_priors]
     conf[best_truth_overlap < threshold] = 0  # label as background
     loc = encode(matches, priors, variances)
-    print(loc.sum())
+    # print(loc.sum())
     loc_t[idx] = loc    # [num_priors,4] encoded offsets to learn
     conf_t[idx] = conf  # [num_priors] top class label for each prior
 
@@ -135,6 +135,7 @@ def encode(matched, priors, variances):
     g_cxcy = (matched[:, :2] + matched[:, 2:])/2 - priors[:, :2]
     # encode variance
     g_cxcy /= (variances[0] * priors[:, 2:])
+    print(g_cxcy)
     # match wh / prior wh
     g_wh = (matched[:, 2:] - matched[:, :2]) / priors[:, 2:]
     g_wh = torch.log(g_wh) / variances[1]
