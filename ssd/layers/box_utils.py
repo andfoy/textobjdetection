690c7780497aa4213532469e4497bc1102bcab88
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-27 16:19:13 -0500

    Training: Debugging

--- ssd/layers/box_utils.py
+++ ssd/layers/box_utils.py
@@ -113,7 +113,7 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
     conf = labels[best_truth_idx] + 1         # Shape: [num_priors]
     conf[best_truth_overlap < threshold] = 0  # label as background
     loc = encode(matches, priors, variances)
-    print(loc)
+    print(loc.sum())
     loc_t[idx] = loc    # [num_priors,4] encoded offsets to learn
     conf_t[idx] = conf  # [num_priors] top class label for each prior
 
