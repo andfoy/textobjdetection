94cea77e7985a9315d34b042a29018b4f5e42a51
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-27 16:15:40 -0500

    Training: Debugging

--- ssd/layers/box_utils.py
+++ ssd/layers/box_utils.py
@@ -94,7 +94,7 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
         truths,
         point_form(priors)
     )
-    print(overlaps)
+    print(overlaps.sum())
     # (Bipartite Matching)
     # [1,num_objects] best prior for each ground truth
     best_prior_overlap, best_prior_idx = overlaps.max(1)
