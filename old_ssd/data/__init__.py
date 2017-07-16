360d9902ca814f4d1f116877be9a77e202ab6d64
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-07-15 21:08:55 -0500

    Old SSD definition removed

--- old_ssd/data/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .voc import VOCDetection, AnnotationTransform, detection_collate, VOC_CLASSES
-from .voc0712 import VOCDetection, AnnotationTransform, detection_collate, VOC_CLASSES
-from .data_augment import *
-from .config import *
