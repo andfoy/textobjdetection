4ebe1f52bc45ec187bf7a2efdf9e9e6171e1e238
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-07-15 22:04:54 -0500

    What's going on?

--- ssd/layers/box_utils.py
+++ ssd/layers/box_utils.py
@@ -104,7 +104,7 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
         best_truth_idx[best_prior_idx[j]] = j
     matches = truths[best_truth_idx]          # Shape: [num_priors,4]
     conf = labels[best_truth_idx] + 1         # Shape: [num_priors]
-    conf[best_truth_overlap < threshold] = 151  # label as background
+    conf[best_truth_overlap < threshold] = 0  # label as background
     loc = encode(matches, priors, variances)
     loc_t[idx] = loc    # [num_priors,4] encoded offsets to learn
     conf_t[idx] = conf  # [num_priors] top class label for each prior
